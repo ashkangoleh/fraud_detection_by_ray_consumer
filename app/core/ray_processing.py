5d67680ff6294b846f09ec3b1ef81c6771ce68d5
ashkan <ashkan.g@bit24.info>
2024-09-18 11:40:36 +0330

    modified:   app/core/ray_processing.py 	modified:   docker-compose.yml

--- app/core/ray_processing.py
+++ app/core/ray_processing.py
@@ -9,18 +9,18 @@ from app.core.fraud_detection import FraudDetection
 @ray.remote
 class TransactionConsumer:
     def __init__(self):
-        # Initialize the FraudDetection system (this is likely serializable)
+        
         self.fraud_detector = FraudDetection(
             neo4j_uri="bolt://neo4j:7687",
             neo4j_user="neo4j",
             neo4j_password="password"
         )
-        self.consumer = None  # Don't initialize the Kafka consumer here
+        self.consumer = None  
 
     def _initialize_consumer(self):
         """ Lazily initialize the Kafka Consumer when needed. """
         if self.consumer is None:
-            from confluent_kafka import Consumer  # Move import here
+            from confluent_kafka import Consumer  
             conf = {
                 'bootstrap.servers': 'kafka1:9092,kafka2:9093',
                 'group.id': 'fraud_detection_group',
@@ -31,29 +31,28 @@ class TransactionConsumer:
 
     def consume_transactions(self):
         """ Function to consume messages from Kafka and process them. """
-        self._initialize_consumer()  # Initialize Kafka consumer on first use
+        self._initialize_consumer()  
 
         try:
             while True:
-                msg = self.consumer.poll(timeout=1.0)  # Poll messages with a timeout
-                print(f"==>> msg: {msg}")
+                msg = self.consumer.poll(timeout=1.0)
                 if msg is None:
-                    continue  # No new message, keep polling
+                    continue  
                 if msg.error():
                     print(f"Consumer error: {msg.error()}")
                     continue
 
-                # Deserialize the Kafka message value
+                
                 transaction = json.loads(msg.value())
                 print(f"Received transaction: {transaction}")
 
-                # Process the transaction (use fraud detection logic here)
+                
                 if transaction:
                     self.process_transaction(transaction)
         except Exception as e:
             print(f"Error while consuming transactions: {e}")
         finally:
-            # Close the consumer to clean up resources
+            
             if self.consumer:
                 self.consumer.close()
 
@@ -72,7 +71,7 @@ class TransactionConsumer:
                 'rapid_transactions': rapid_transactions.to_dict(orient='records') if not rapid_transactions.empty else [],
                 'suspicious_users': suspicious_users if suspicious_users else []
             }
-            # Handle the alert (e.g., log it, send a notification)
+            
             logging.warning("Alert: " + str({
                 'alert': alert,
                 'created_at': datetime.now().isoformat()
